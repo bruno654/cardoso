e4481ae6a0c2ffc33e3cda38056123ba9ae10b5a
bruno654 <67012261+bruno654@users.noreply.github.com>
2023-12-06 00:31:04 +0000

    fix: testando admin

--- app/components/Aside.tsx
+++ app/components/Aside.tsx
@@ -1,67 +1,25 @@
-import { Button } from "@/components/ui/button";
-import { Bars4Icon, HomeIcon, ListBulletIcon, NewspaperIcon } from "@heroicons/react/24/outline";
+import { Bars4Icon, NewspaperIcon } from "@heroicons/react/24/outline";
 
 export default function Aside() {
-    return (
-        <>
+  return (
+    <div className="w-1/4 border-r">
+      <div>
+        <h2>Livro</h2>
+        <a href="/admin/books">
+          <div className="flex items-center hover:text-gray-500">
+            <Bars4Icon className="h-4 mr-2" />
+            <span>Listar Livros</span>
+          </div>
+        </a>
+        <br />
 
-            <div className="w-1/4 border-r">
-                <div>
-                    <h2>Dashboard</h2>
-                    <a href="/admin">
-                        <Button variant="ghost">
-                            <HomeIcon className="h-4 mr-2" />
-                            Home
-                        </Button>
-                    </a>
-                    <br />
-
-                    <a href="/admin/course">
-                        <Button variant="ghost">
-                            <Bars4Icon className="h-4 mr-2" />
-                            Listar Curso
-                        </Button>
-                    </a>
-                    <br />
-                    <a href="/admin/course/new">
-                        <Button variant="ghost">
-                            <NewspaperIcon className="h-4 mr-2" />
-                            Cadastrar Curso
-                        </Button>
-                    </a>
-                </div>
-            
-
-
-            <div>
-                <h2>Configurações</h2>
-                <a href="/admin">
-                    <Button variant="ghost">
-                        <HomeIcon className="h-4 mr-2" />
-                        Home
-                    </Button>
-                </a>
-                <br />
-
-                <a href="/admin/course">
-                    <Button variant="ghost">
-                        <Bars4Icon className="h-4 mr-2" />
-                        Listar Curso
-                    </Button>
-                </a>
-                <br />
-                <a href="/admin/course/new">
-                    <Button variant="ghost">
-                        <NewspaperIcon className="h-4 mr-2" />
-                        Cadastrar Curso
-                    </Button>
-                </a>
-            </div>
-        </div>
-        </>
-
-
-
-
-    )
+        <a href="/admin/books/new">
+          <div className="flex items-center hover:text-gray-500">
+            <NewspaperIcon className="h-4 mr-2" />
+            <span>Cadastrar Livro</span>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
 }
