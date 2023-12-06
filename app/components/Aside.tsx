b32242008b9feea0533347528d878c4d847c7ae0
bruno654 <67012261+bruno654@users.noreply.github.com>
2023-12-06 01:36:33 +0000

    fix: teste

--- app/components/Aside.tsx
+++ app/components/Aside.tsx
@@ -1,23 +1,21 @@
-import { Bars4Icon, NewspaperIcon } from "@heroicons/react/24/outline";
+import { Bars4Icon, HomeIcon, NewspaperIcon } from "@heroicons/react/24/outline";
 
 export default function Aside() {
   return (
     <div className="w-1/4 border-r">
       <div>
         <h2>Livro</h2>
-        <a href="/admin/books">
-          <div className="flex items-center hover:text-gray-500">
-            <Bars4Icon className="h-4 mr-2" />
-            <span>Listar Livros</span>
-          </div>
+        <a href="/admin">
+          
         </a>
         <br />
 
-        <a href="/admin/books/new">
-          <div className="flex items-center hover:text-gray-500">
-            <NewspaperIcon className="h-4 mr-2" />
-            <span>Cadastrar Livro</span>
-          </div>
+        <a href="/admin/course">
+          
+        </a>
+        <br />
+        <a href="/admin/course/new">
+          
         </a>
       </div>
     </div>
